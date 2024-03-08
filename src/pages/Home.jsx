697470fc2d9c8d14106e0219baf4bf97b2ae46cb
Asammour <areej_sammour@hotmail.com>
2024-03-08 22:47:16 +0100

    commit17

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     console.log('API URL:', process.env.REACT_APP_API_URL);
     const fetchData = async () => {
       try {
-        const res = await axios.get(`${process.env.REACT_APP_API_URL}posts${cat}`);
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/posts${cat}`);
         console.log('Received data:', res.data);
         setPosts(res.data);
       } catch (err) {
