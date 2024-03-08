ca1fc887e9ac22fa51b5250ae1f9f03fe58106a9
Asammour <areej_sammour@hotmail.com>
2024-03-08 22:04:09 +0100

    commit7

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     console.log('API URL:', process.env.REACT_APP_API_URL);
     const fetchData = async () => {
       try {
-        const res = await axios.get(`${process.env.REACT_APP_API_URL}/posts${cat}`);
+        const res = await axios.get(`https://jaktharry-app-130a026cb2e4.herokuapp.com/posts${cat}`);
         console.log('Received data:', res.data);
         setPosts(res.data);
       } catch (err) {
