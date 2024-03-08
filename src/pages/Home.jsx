7265209549ec4f5d38b081ceee4e4bf941cff7bf
Asammour <areej_sammour@hotmail.com>
2024-03-08 22:22:12 +0100

    commit11

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,9 +15,15 @@ const Home = () => {
     console.log('API URL:', process.env.REACT_APP_API_URL);
     const fetchData = async () => {
       try {
-        const res = await axios.get(`${process.env.REACT_APP_API_URL}/posts${cat}`);
+        const res = await axios.get(`${process.env.PUBLIC_URL}/posts${cat}`);
         console.log('Received data:', res.data);
         setPosts(res.data);
+        if (Array.isArray(res.data)) {
+          setPosts(res.data);
+        } else {
+          console.error('Invalid data format:', typeof res.data);
+          setError('Invalid data format. Please try again later.');
+        }
       } catch (err) {
         console.error('Error fetching data:', err);
         setError('Error fetching data. Please try again later.');
