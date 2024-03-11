b6af64380fdace3dd572ca5ccb317590516e90d1
Asammour <areej_sammour@hotmail.com>
2024-03-11 21:43:03 +0100

    m7

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -6,9 +6,14 @@ export const AuthContext = createContext()
 export const AuthContextProvider = ({children})=>{
   const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user")|| null))
 
-  const login = async(inputs)=>{
-    const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login`, inputs);
-    setCurrentUser(res.data);
+  const login = async (inputs) => {
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login`, inputs);
+      setCurrentUser(res.data);
+    } catch (error) {
+      // Handle error (e.g., show a message to the user)
+      console.error('Login failed:', error.message);
+    }
   };
 
   const logout = async(inputs)=>{
