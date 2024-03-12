378b3ff119ec1a7772421fcde39d26ec52ae8eb4
Asammour <areej_sammour@hotmail.com>
2024-03-12 14:09:18 +0100

    t16

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -31,7 +31,7 @@ const Menu = ({cat}) => {
       {posts.map((post)=>(
         <div className="post" key={post.id}>
           <img src={`../upload/${post?.img}`} alt="" />
-          <h2>{post.title}</h2>
+          <h3>{post.title}</h3>
           <Button onClick={() => handleClick(post.id)}>Läs mer</Button>
         </div>
       ))}
