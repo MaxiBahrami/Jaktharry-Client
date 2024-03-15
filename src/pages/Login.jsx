0340ff59e236d828713a3443dd05080fa563ec0e
Asammour <areej_sammour@hotmail.com>
2024-03-15 23:06:20 +0100

    currentuser6

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -24,35 +24,20 @@ const Login = () => {
   const handleSubmit = async e =>{
     e.preventDefault();
     try{
-      const { username, password } = inputs;
-      const response = await login({ username, password });
-      const { token } = response.data;
-      
-      // Store the token in localStorage
-      localStorage.setItem('access_token', token);
-      
+      await login(inputs);
       navigate("/");
-    }catch(error) {
-    if (error.response) {
-      // Server responded with an error status code
-      setError(error.response.data.message);
-    } else if (error.request) {
-      // The request was made but no response was received
-      setError("Server did not respond. Please try again later.");
-    } else {
-      // Something else happened in making the request
-      setError("An unexpected error occurred. Please try again later.");
+    }catch(err){
+      setError(err.response.data);
     }
-  }
   };
 
   return (
     <Container className='auth'>
       <h1>Logga in</h1>
-      <form onSubmit={handleSubmit}>
+      <form >
         <input required type="text" placeholder='Användarnamn ' name="username" onChange={handleChange}/>
         <input required type="password " placeholder='Lösenord' name="password" onChange={handleChange}/>
-        <Button type="submit">Logga in</Button>
+        <Button onClick={handleSubmit}>Logga in</Button>
         {err && <p>{err}</p>}
         <span>Har du inget konto?.. <Link to="/register">Registrera</Link></span>
       </form>
