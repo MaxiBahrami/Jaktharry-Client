647fe3a5d129857812d328381aa88339ed360e59
Asammour <areej_sammour@hotmail.com>
2024-03-16 22:42:21 +0100

    admin5

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Login from "./pages/Login";
 import Write from "./pages/Write";
 import Home from "./pages/Home";
 import Single from "./pages/Single";
-import Admindash from "./pages/Admindash";
+import Admindash from "./pages/Admindash.jsx";
 import Footer from "./components/Footer";
 import CustomNavbar from "./components/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
